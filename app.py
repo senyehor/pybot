cbafea0cbe7438b33efea255134093d5d5b560cf
Yehor <e.sencha2002@gmail.com>
2021-07-11 14:45:24 +0300

    changed updates processing

--- app.py
+++ app.py
@@ -2,28 +2,25 @@ import os
 
 import telegram
 from flask import Flask, request, Response
-from telegram.ext import Updater, Dispatcher
+from telegram.ext import Dispatcher
 
-# from bot import main_endless_conversation
-# from test import pog
+from bot import main_endless_conversation
 
 app = Flask(__name__)
-
 
 PORT = int(os.getenv('PORT'))
 BOT_TOKEN = os.getenv('bot_token')
 BOT_USERNAME = os.getenv('bot_username')
 BOT_URL_PATH = os.getenv('bot_url_path')
 BOT = telegram.Bot(BOT_TOKEN)
-UPDATER = Updater(BOT_TOKEN, use_context=True)
-DISPATCHER: Dispatcher = UPDATER.dispatcher
-# if not DISPATCHER.handlers:
-#     DISPATCHER.add_handler()
-print(__name__)
+DISPATCHER = Dispatcher(BOT, None, workers=0)  # noqa
+if not DISPATCHER.handlers:
+    DISPATCHER.add_handler(main_endless_conversation)
+
 
 @app.route(f'/{BOT_TOKEN}', methods=['POST', 'GET'])
-def hooks_getter():
-    """Bot sends hooks every time he gets a response and this func passes them to dispatcher"""
+def webhooks_getter():
+    """Bot sends hooks every time he gets a message and this func passes them to dispatcher"""
     try:
         update = telegram.Update.de_json(request.get_json(force=True), BOT)
         DISPATCHER.process_update(update)
